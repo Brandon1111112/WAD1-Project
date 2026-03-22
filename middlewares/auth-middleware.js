e084ccc4c76311023f39399f1bbf7cc69fc47ffb
KimoqGit <205516002+KimoqGit@users.noreply.github.com>
2026-03-22 20:24:06 +0800

    small bug fix in isAdmin middleware

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -15,6 +15,7 @@ const isAdmin = (req,res,next) => {
         console.log('Not an admin user, redirecting to /home')
         return res.redirect('/home')
     }
+    next();
 }   
 
 module.exports = {
